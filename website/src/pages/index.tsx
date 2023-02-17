ee66d834359a57b131e85e2ea2e0d52604f5cf3d
romitkarmakar <romitkarmakar@gmail.com>
2023-02-18 03:09:40 +0530

    Added components

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,7 +1,27 @@
+import Blogs from "@/components/Blogs";
+import Clients from "@/components/Clients";
+import ContactForm from "@/components/ContactForm";
+import FAQ from "@/components/Faq";
+import Features from "@/components/Features";
+import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
 import Pricing from "@/components/Pricing";
+import Stats from "@/components/Stats";
 
 export default function HomePage() {
-  return <div>
-    <Pricing />
-  </div>
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Clients />
+      <Stats />
+      <Features />
+      <Pricing />
+      <Blogs />
+      <FAQ />
+      <ContactForm />
+      <Footer />
+    </div>
+  );
 }
