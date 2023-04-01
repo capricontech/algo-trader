b92c4080d8837b0f53cd07bdbb8816ab837505ad
romitkarmakar <romitkarmakar@gmail.com>
2023-04-01 21:30:30 +0530

    Added form submission

--- website/src/components/ContactForm.tsx
+++ website/src/components/ContactForm.tsx
@@ -4,6 +4,28 @@ import { classNames } from "@/lib/helpers";
 
 export default function ContactForm() {
   const [agreed, setAgreed] = useState(false);
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    amount: "",
+  });
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    fetch("https://eonmd95g51ib4i7.m.pipedream.net", {
+      method: "POST",
+      body: JSON.stringify(data),
+    }).then((res) => {
+      if (res.status === 200) {
+        alert("Your message has been sent! Please wait for 1-2 business days.");
+      } else {
+        alert("Error! Please try again later.");
+      }
+    });
+  };
 
   return (
     <div className="bg-white py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24">
@@ -102,6 +124,10 @@ export default function ContactForm() {
                   name="first-name"
                   id="first-name"
                   autoComplete="given-name"
+                  value={data.firstName}
+                  onChange={(e) =>
+                    setData({ ...data, firstName: e.target.value })
+                  }
                   className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
                 />
               </div>
@@ -119,6 +145,10 @@ export default function ContactForm() {
                   name="last-name"
                   id="last-name"
                   autoComplete="family-name"
+                  value={data.lastName}
+                  onChange={(e) =>
+                    setData({ ...data, lastName: e.target.value })
+                  }
                   className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
                 />
               </div>
@@ -136,6 +166,8 @@ export default function ContactForm() {
                   name="email"
                   type="email"
                   autoComplete="email"
+                  value={data.email}
+                  onChange={(e) => setData({ ...data, email: e.target.value })}
                   className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
                 />
               </div>
@@ -167,6 +199,10 @@ export default function ContactForm() {
                   name="phone-number"
                   id="phone-number"
                   autoComplete="tel"
+                  value={data.phoneNumber}
+                  onChange={(e) =>
+                    setData({ ...data, phoneNumber: e.target.value })
+                  }
                   className="py-3 px-4 block w-full pl-20 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
                   placeholder="+91 (555) 987-6543"
                 />
@@ -197,6 +233,8 @@ export default function ContactForm() {
                   type="number"
                   name="amount"
                   id="amount"
+                  value={data.amount}
+                  onChange={(e) => setData({ ...data, amount: e.target.value })}
                   className="py-3 px-4 block w-full pl-20 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
                   placeholder="1,05,000"
                 />
@@ -241,6 +279,7 @@ export default function ContactForm() {
             <div className="sm:col-span-2">
               <button
                 type="submit"
+                onClick={handleSubmit}
                 className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 Let's talk
