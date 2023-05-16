1fc8fba13c90f7d6620c92e998f930231b5a0a28
romitkarmakar <romitkarmakar@gmail.com>
2023-05-16 11:47:27 +0530

    Fixed bug

--- website/src/lib/helpers.ts
+++ website/src/lib/helpers.ts
@@ -3,5 +3,6 @@ export function classNames(...classes: (string | undefined)[]) {
 }
 
 export function isValidEmail(email: string) {
+  if (!email) return false;
   return email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
 }
